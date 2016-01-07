2a582452c0bd60b8ef8c1312c1adc38ea4a935e9
Ashok Madudru <ashokgirihari@gmail.com>
2016-01-07 22:08:41 +0530

    change splash name to SignIn. Add signin xml design.

--- app/src/main/java/com/dsquare/hibour/activities/SignIn.java
+++ app/src/main/java/com/dsquare/hibour/activities/SignIn.java
@@ -3,17 +3,15 @@ package com.dsquare.hibour.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.dsquare.hibour.R;
 
-public class Splash extends AppCompatActivity {
+public class SignIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_signin);
 
         Intent signupIntent = new Intent(this,SignUp.class);
         startActivity(signupIntent);
