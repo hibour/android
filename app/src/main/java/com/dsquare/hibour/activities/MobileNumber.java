bbb1e6de3337d16ff18569b30961e3071fb7e21c
AshokMadduru <ashokgirihari@gmail.com>
2016-01-25 16:10:27 +0530

    phone number screen and otp sceren created

---
+++ app/src/main/java/com/dsquare/hibour/activities/MobileNumber.java
@@ -0,0 +1,15 @@
+package com.dsquare.hibour.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dsquare.hibour.R;
+
+public class MobileNumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile_number);
+    }
+}
