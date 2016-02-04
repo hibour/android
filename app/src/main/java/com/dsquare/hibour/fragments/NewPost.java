1b02c44fcfc5fdf1dc3aba0802e0d067e8557ca0
Ashok <ashok@dsquarelabs.com>
2016-02-04 15:35:36 +0530

    resolved crash problem

--- app/src/main/java/com/dsquare/hibour/fragments/NewPost.java
+++ app/src/main/java/com/dsquare/hibour/fragments/NewPost.java
@@ -2,32 +2,24 @@ package com.dsquare.hibour.fragments;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.widget.RecyclerView;
 import android.util.Base64;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
-import android.util.Base64;
-import android.util.Log;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.TranslateAnimation;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -40,15 +32,12 @@ import android.widget.Toast;
 
 import com.android.volley.VolleyError;
 import com.dsquare.hibour.R;
-import com.dsquare.hibour.adapters.CategoriesRecycler;
 import com.dsquare.hibour.dialogs.PostsImagePicker;
 import com.dsquare.hibour.interfaces.ImagePicker;
 import com.dsquare.hibour.interfaces.WebServiceResponseCallback;
 import com.dsquare.hibour.network.NetworkDetector;
 import com.dsquare.hibour.network.PostsClient;
 import com.dsquare.hibour.pojos.neighours.Neighourhoodpojo;
-import com.dsquare.hibour.pojos.posttype.Datum;
-import com.dsquare.hibour.pojos.posttype.PostTypeCatg;
 import com.dsquare.hibour.utils.Constants;
 import com.dsquare.hibour.utils.Fonts;
 import com.dsquare.hibour.utils.Hibour;
@@ -79,7 +68,6 @@ public class NewPost extends android.support.v4.app.Fragment implements View.OnC
     private TextView done,cancel;
     private ArrayAdapter<String> categoriesAdapter;
     private ArrayAdapter<String> neighoursAdapter;
-    private String[] List = {"General", "Suggestions", "Classifieds", "Crime & Safety", "Lost & Found"};
     private static final int REQUEST_IMAGE_SELECTOR = 1000;
     private static final int REQUEST_IMAGE_CAPTURE = 1001;
     private java.util.List<String> categoriesList = new ArrayList<>();
@@ -103,8 +91,24 @@ public class NewPost extends android.support.v4.app.Fragment implements View.OnC
     private ListView categoriesRecycler;
     private String[] details;
     private String catFrmPre="";
+
+    public interface PostsListener{
+        void onCancelClicked();
+        void onDoneClicked();
+    }
+    private PostsListener mListener;
     public NewPost() {
         // Required empty public constructor
+    }
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (PostsListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement NoticeDialogListener");
+        }
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -123,6 +127,7 @@ public class NewPost extends android.support.v4.app.Fragment implements View.OnC
         done.setOnClickListener(this);
         gallary.setOnClickListener(this);
         delete.setOnClickListener(this);
+        cancel.setOnClickListener(this);
         editPost.requestFocus();
         /*editPost.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -147,6 +152,8 @@ public class NewPost extends android.support.v4.app.Fragment implements View.OnC
             case R.id.create_delete_image:
                 opendeleteImage();
                 break;
+            case R.id.creat_post_cancel:
+                mListener.onCancelClicked();
         }
     }
 
@@ -289,6 +296,7 @@ public class NewPost extends android.support.v4.app.Fragment implements View.OnC
         try {
             JSONObject data = jsonObject.getJSONObject("data");
             Toast.makeText(getActivity(), "Post update successfully", Toast.LENGTH_LONG).show();
+            mListener.onDoneClicked();
         } catch (JSONException e) {
             e.printStackTrace();
             Toast.makeText(getActivity(), "Post updation failed", Toast.LENGTH_LONG).show();
@@ -312,7 +320,7 @@ public class NewPost extends android.support.v4.app.Fragment implements View.OnC
         neighboursSpinner = (Spinner)view.findViewById(R.id.newpost_neighbourhood_spinner);
         editPost = (EditText) view.findViewById(R.id.newposts_edittest);
         done = (TextView) view.findViewById(R.id.create_post_done);
-        // cancel = (TextView) view.findViewById(R.id.creat_post_cancel);
+         cancel = (TextView) view.findViewById(R.id.creat_post_cancel);
         gallary = (ImageView) view.findViewById(R.id.creat_imageview_post_icon);
         postImage = (ImageView) view.findViewById(R.id.creat_imageview_display_icon);
         delete = (ImageView) view.findViewById(R.id.create_delete_image);
@@ -341,7 +349,7 @@ public class NewPost extends android.support.v4.app.Fragment implements View.OnC
                     String categoriesType = categoriesList.get(position);
                     Log.d("categoriestype", categoriesType);
                     if (!categoriesType.equals("Select Categories")) {
-                        categoriesTypeId = categoriesMap.get(categoriesType);
+                        categoriesTypeId = Constants.postTypesMap.get(categoriesType).get("id");
                         Log.d("categoriestype", categoriesType);
                     }
                     ((TextView) parent.getChildAt(0)).setTextColor(getResources()
@@ -463,15 +471,17 @@ public class NewPost extends android.support.v4.app.Fragment implements View.OnC
             int i=0;
             int j=0;
             for(String type:Constants.postTypesMap.keySet()){
-                categoriesList.add(type);i++;
+                categoriesList.add(type);
                 if(catFrmPre.equals(type)){
                     j=i;
                 }
+                i++;
             }
             categoriesAdapter = new ArrayAdapter<String>(getActivity()
                     , android.R.layout.simple_dropdown_item_1line, categoriesList);
             categoriesSpinner.setAdapter(categoriesAdapter);
             categoriesSpinner.setSelection(j);
+            categoriesTypeId = Constants.postTypesMap.get(catFrmPre).get("id");
         }
     }
 
